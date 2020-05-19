754c6f2a4ba32af5960e016fc68a2c55b2f5f592
Roman Pupko <60660149+Lelious@users.noreply.github.com>
2020-05-19 17:35:40 +0300

    Sushi_bot_Interface

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+using Serilog;
+
+namespace Sushi_bot
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Log.Logger = new LoggerConfiguration()
+               .MinimumLevel.Debug()
+               .Enrich.FromLogContext()             
+               .WriteTo.File($"logs\\log.txt", rollingInterval: RollingInterval.Day, fileSizeLimitBytes: 30000)
+               .CreateLogger();
+
+            Log.Information($"Starting program: {Application.CompanyName}, number of Therad : {AppDomain.GetCurrentThreadId()}");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SushiBot());
+        }
+    }
+}
